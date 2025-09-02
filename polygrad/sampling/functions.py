28875c3c12cc2d001510420dd4c2305d2255c71a
danielefoffano <daniele0995@hotmail.it>
2025-09-02 11:48:44 +0200

    sigmoid advantage

--- polygrad/sampling/functions.py
+++ polygrad/sampling/functions.py
@@ -224,12 +224,14 @@ def policy_guided_sample_fn(
             + 0.99 * (1.0 - binary_terminals[:, :-1]) * state_value[:, 1:]
         )
 
-    negative_abs_advantage = -torch.abs(advantage)
-    
-    #negative_log_sigm = F.logsigmoid(-advantage)
+    #negative_sq_advantage = -torch.square(advantage)
 
-    #adv_grad = torch.autograd.grad([negative_log_sigm.sum()], [states])[0]
-    adv_grad = torch.autograd.grad([negative_abs_advantage.sum()], [states])[0]
+    #negative_abs_advantage = -torch.abs(advantage)
+    
+    negative_log_sigm = F.logsigmoid(-advantage)
+
+    adv_grad = torch.autograd.grad([negative_log_sigm.sum()], [states])[0]
+    #adv_grad = torch.autograd.grad([negative_sq_advantage.sum()], [states])[0]
     grad = torch.zeros_like(adv_grad)
     grad[:,:-1,:] = adv_grad[:,:-1,:]
     UGV = torch.einsum('ij,bjk,kl->bil', U, grad.float(), V)
